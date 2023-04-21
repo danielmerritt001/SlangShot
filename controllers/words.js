d1f1de580c0a7774afdfa2e1e1762411f81908ff
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-21 14:13:30 -0400

    add NEW route to form

--- controllers/words.js
+++ controllers/words.js
@@ -1,5 +1,28 @@
 import { Word } from '../models/word.js'
 
+function newWord(req, res) {
+  res.render('words/new', {
+    title: "Add Word",
+  })
+}
+
+function index(req, res) {
+  Word.find({})
+  .populate('owner')
+  .then(words => {
+    console.log(words);
+    res.render('words/index', {
+      words,
+      title: "My Words"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
-  
+  newWord as new,
+  index,
 }
--- routes/words.js
+++ routes/words.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import * as wordsCtrl from '../controllers/words.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
+router.get('/new', isLoggedIn, wordsCtrl.new)
+router.get('/index', isLoggedIn, wordsCtrl.index)
 
 export {
   router
