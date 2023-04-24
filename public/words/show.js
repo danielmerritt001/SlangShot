c830620029480dc1650e6dc0129bbaf4576f6035
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-24 15:55:42 -0400

    fix home page

--- public/words/show.js
+++ public/words/show.js
@@ -1,9 +1,10 @@
-let boing = new Audio('/assets/audio/losingsounds/boing.mp3')
-let duck = new Audio('/assets/audio/losingsounds/duck.mp3')
-let sike = new Audio('/assets/audio/losingsounds/sike.mp3')
-let trumpet = new Audio('/assets/audio/losingsounds/trumpet.mp3')
-let wahWah = new Audio('/assets/audio/losingsounds/wah-wah.mp3')
+let boing = new Audio('/assets/audio/wrong-sounds/boing.mp3')
+let duck = new Audio('/assets/audio/wrong-sounds/duck.mp3')
+let sike = new Audio('/assets/audio/wrong-sounds/sike.mp3')
+let trumpet = new Audio('/assets/audio/wrong-sounds/trumpet.mp3')
+let wahWah = new Audio('/assets/audio/wrong-sounds/wah-wah.mp3')
 let lossArray=[boing, duck, sike, trumpet, wahWah]
+let success = new Audio('/assets/audio/correct-sounds/success.mp3')
 
 let tableEl = document.getElementById('definitions')
 let correctEl = document.getElementById('def-0')
@@ -15,7 +16,7 @@ function guess() {
     correctEl.className = "chosen"
     tableEl.removeEventListener('click', guess)
     if(event.target.id==="def-0") {
-
+      playWinSound()
     } else{
       playLossSound()
     }
@@ -25,4 +26,9 @@ function playLossSound() {
   randNum= Math.floor(Math.random()*lossArray.length)
   lossArray[randNum].volume = .8
   lossArray[randNum].play()
+}
+
+function playWinSound() {
+  success.volume = .8
+  success.play()
 }
