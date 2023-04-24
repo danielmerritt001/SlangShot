abb936bb8dfd1ce31cad2d6d6ed1a476551286f7
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-24 15:07:05 -0400

    implement education audio aids

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Slingshot favicon
 https://icons8.com/icons/set/slingshot
 https://slangshot.fly.dev/
 
-
+https://www.freesoundslibrary.com/sike-sound-effect/ By SPANAC
--- public/words/show.js
+++ public/words/show.js
@@ -1,3 +1,10 @@
+let boing = new Audio('/assets/audio/losingsounds/boing.mp3')
+let duck = new Audio('/assets/audio/losingsounds/duck.mp3')
+let sike = new Audio('/assets/audio/losingsounds/sike.mp3')
+let trumpet = new Audio('/assets/audio/losingsounds/trumpet.mp3')
+let wahWah = new Audio('/assets/audio/losingsounds/wah-wah.mp3')
+let lossArray=[boing, duck, sike, trumpet, wahWah]
+
 let tableEl = document.getElementById('definitions')
 let correctEl = document.getElementById('def-0')
 tableEl.addEventListener('click', guess)
@@ -7,6 +14,15 @@ function guess() {
     event.target.className = "chosen"
     correctEl.className = "chosen"
     tableEl.removeEventListener('click', guess)
-    console.log(event.target)
+    if(event.target.id==="def-0") {
+
+    } else{
+      playLossSound()
+    }
   }
+}
+function playLossSound() {
+  randNum= Math.floor(Math.random()*lossArray.length)
+  lossArray[randNum].volume = .8
+  lossArray[randNum].play()
 }
